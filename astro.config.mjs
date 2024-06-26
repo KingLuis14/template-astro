e2ce6bc54620bfd1a9ae276f5a0408f75db81ad2
Luis Enrique <enriquecalero284@gmail.com>
2024-06-25 23:35:28 -0500

    postcss y purge configurados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar:{
+  devToolbar: {
     enabled: false
   },
-  integrations: [icon()]
+  integrations: [icon(), purgecss({
+    extractors : [
+      {
+        extractor: (content) => 
+          content.match(/[^<>"'`\s]*[@:][^<>"'`\s]*|[^<>"'`\s]*[^<>"'`\s]/g) || [],
+          extensions: ["astro", "html"],
+      }
+    ]
+  })]
 });
